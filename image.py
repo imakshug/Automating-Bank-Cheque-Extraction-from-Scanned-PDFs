c14f4b43d1711214921ab163181f724e86dec1f0
imakshug <akarni213@gmail.com>
2024-05-06 19:23:11 +0530

    chng

---
+++ image.py
@@ -0,0 +1,18 @@
+import pytesseract
+from PIL import Image
+
+# Set the path to the Tesseract executable
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\akarn\pytesseract-0.3.10\pytesseract'
+
+def extract_text_from_image(image_path):
+    # Open the image file
+    with Image.open(image_path) as img:
+        # Perform OCR on the image
+        text = pytesseract.image_to_string(img)
+    return text
+
+# Example usage
+image_path = "C:\Automating-Bank-Check-Extraction-from-Scanned-PDFs_Apr_2024\output\page_1.jpg"  # Path to the image file
+extracted_text = extract_text_from_image(image_path)
+print("Extracted Text:")
+print(extracted_text)
---
+++ pdf_to_image.py
@@ -0,0 +1,12 @@
+from pdf2image import convert_from_path
+
+def pdf_to_image(pdf_path, output_folder):
+    pages = convert_from_path(pdf_path)
+    for i, page in enumerate(pages):
+        page.save(f"{output_folder}/page_{i+1}.jpg", "JPEG")
+
+# Example usage
+pdf_file = "C:\Automating-Bank-Check-Extraction-from-Scanned-PDFs_Apr_2024\cheque1.pdf"  # Path to your PDF file
+output_folder = "output"  # Output folder where images will be saved
+
+pdf_to_image(pdf_file, output_folder)
