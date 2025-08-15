e84a68ca8d958d22c3caa8d5223db5e2978e79eb
Akshita Gupta <akarni213@gmail.com>
2025-08-16 00:14:46 +0530

    update

--- run_web_dashboard.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d "%~dp0"
-call .\venv_new\Scripts\Activate.ps1
-echo Starting Flask Web Server...
-echo.
-echo Dashboard will be available at: http://localhost:5000
-echo.
-python flask_app.py
-pause
